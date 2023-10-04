416a956ccf4c84e5db660908f9aa5e741d6dc73d
Asfandyar <a_asfandyar1@hotmail.com>
2023-10-04 17:23:36 +0100

    Project complete

---
+++ Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Mac User on 04/10/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
